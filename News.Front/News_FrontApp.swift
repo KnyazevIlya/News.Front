2951bd7f1a007ce64cc8bea6e65b2efe4a706bd8
Ilya Knyazev <knyazevilya03@gmail.com>
2022-11-27 01:07:30 +0200

    Initial Commit

---
+++ News.Front/News_FrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_FrontApp.swift
+//  News.Front
+//
+//  Created by kniaziev.intern on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct News_FrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
